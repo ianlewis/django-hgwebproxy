22b369512eca5bb83de072689e5070d8408039c7
faheem@wsgi.silkworm <none@none>
2008-09-05 14:46:16 -0400

    Fixes to make admin work for Django 1.0.

--- urls.py
+++ urls.py
@@ -11,10 +11,14 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 #from views import hgview
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^hg/login/$', 'django.contrib.auth.views.login', {'template_name':'registration/login.html'}, name='login'),
                        url(r'^hg/logout/$', 'django.contrib.auth.views.logout', {'template_name':'registration/logout.html'}, name='logout'),
-                       url(r'^hg/admin/', include('django.contrib.admin.urls')),
+                       #url(r'^hg/admin/', include('django.contrib.admin.urls')),
+                       url(r'^hg/admin/(.*)', admin.site.root),
                        url(r'^hg/', include('hg.hgwebproxy.urls')),
+
                        )
