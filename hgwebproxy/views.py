aca0d1812ba7a1582aa2e662abaada5213fffb7a
Micah Ransdell <micahr@spu.edu>
2009-06-26 11:57:03 -0700

    Added Django Authentication for end users using a browser Basic Authentication is now only used for mercurial users

--- hgwebproxy/views.py
+++ hgwebproxy/views.py
@@ -127,6 +127,25 @@ class _hgReqWrap(object):
                 thing = str(thing)
                 self.response.write(thing)
 
+def is_mercurial(request):
+    """
+    User agent processor to determine whether the incoming
+    user is someone using a browser, or a mercurial client
+    
+    In order to qualify as a mercurial user they must have a user
+    agent value that starts with mercurial and an Accept header that
+    starts with application/mercurial. This guarantees we only force
+    those who are actual mercurial users to use Basic Authentication
+    """
+    agent = re.compile(r'^(mercurial).*')
+    accept = request.META.get('HTTP_ACCEPT',None)
+    result = agent.match(request.META.get('HTTP_USER_AGENT',None))
+
+    if result and accept.startswith('application/mercurial-'):
+        return True
+    else:
+        return False
+
 def basic_auth(request, realm):
     """
     Very simple Basic authentication handler which hooks
@@ -169,9 +188,18 @@ def hgroot(request, *args):
     can push.
     """
     #if request.method == "POST":
-    realm = 'Django Basic Auth' # Change me, if you want.
-
-    authed = basic_auth(request, realm)
+    realm = 'Basic Auth' # Change if you want.
+    
+    if is_mercurial(request):
+        # This is a request by a mercurial user
+        authed = basic_auth(request, realm)
+    else:
+        # This is a standard web request
+        if not request.user.is_authenticated():
+            return HttpResponseRedirect('%s?next=%s' % 
+                                        (settings.LOGIN_URL,request.path))
+        else:
+            authed = request.user
 
     if not authed:
         resp.status_code = 401
