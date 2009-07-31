4c7595d08a82e764ac269b8db491051c59a92e39
Mario César <mariocesar.c50@gmail.com>
2009-07-31 14:09:34 -0400

    Added auth views and fix repo list breadcrumb

--- example/urls.py
+++ example/urls.py
@@ -4,8 +4,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
-    (r'^hg/', include('hgwebproxy.urls')),
+    url(r'^admin/repos/', include('hgwebproxy.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'django.contrib.auth.views.login',
+        {'template_name':'registration/login.html'}, name='login'),
+   url(r'^logout/$', 'django.contrib.auth.views.logout',
+        {'template_name':'registration/logout.html'}, name='logout'),
 )
 
 from django.conf import settings
