0b21194bfd2ceb9b931cf56608b7c7039c331027
Ian Lewis <IanMLewis@gmail.com>
2009-12-04 21:04:07 +0900

    Added localization support to the model's verbose name

--- hgwebproxy/models.py
+++ hgwebproxy/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models import permalink
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 class Repository(models.Model):
     name = models.CharField(max_length=140)
@@ -14,8 +15,8 @@ class Repository(models.Model):
         help_text="Same as in hgrc config, as: zip, bz2, gz")
 
     class Meta:
-        verbose_name = 'repository'
-        verbose_name_plural = 'repositories'
+        verbose_name = _('repository')
+        verbose_name_plural = _('repositories')
         ordering = ['name',]
         permissions = (
             ("can_push", "Can Push"),
