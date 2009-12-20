8813aa93b65b0223eec16f05f5fe06b8126a48f7
Ian Lewis <IanMLewis@gmail.com>
2009-12-20 14:05:06 +0900

    Added pull/push permissions to the RepositoryPermissions

--- hgwebproxy/permissions.py
+++ hgwebproxy/permissions.py
@@ -5,5 +5,21 @@ from models import Repository
 
 class RepositoryPermission(BasePermission):
     label = 'repository_permission'
+    checks = ('push', 'pull', 'browse', 'can_push', 'can_pull')
+
+    def can_pull(self, repo):
+        if repo.owner == self.user:
+            return True
+        return self.pull_repository(repo)
+
+    def can_push(self, repo):
+        if repo.owner == self.user:
+            return True
+        return self.push_repository(repo)
+
+    def browse(self, repo):
+        if repo.owner == self.user:
+            return True
+        return self.browse_repository(repo)
 
 authority.register(Repository, RepositoryPermission)
