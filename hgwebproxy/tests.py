2632e6a3f8d7601fcecc80fd47981d6065b2c59f
Ian Lewis <IanMLewis@gmail.com>
2010-01-06 22:17:20 +0900

    Added repository permissions test

---
+++ hgwebproxy/tests.py
@@ -0,0 +1,56 @@
+#:coding=utf-8:
+
+from django.contrib.auth.models import User
+from django.test import TestCase as DjangoTestCase
+from django.core.urlresolvers import reverse
+
+from models import Repository
+
+class PermissionTest(DjangoTestCase):
+    fixtures = ['basic.json']
+
+    def assertReader(self, repo, user):
+        self.assertTrue(repo.can_browse(user))
+        self.assertTrue(repo.can_pull(user))
+        self.assertFalse(repo.can_push(user))
+
+    def assertWriter(self, repo, user):
+        self.assertTrue(repo.can_browse(user))
+        self.assertTrue(repo.can_pull(user))
+        self.assertTrue(repo.can_push(user))
+
+    def test_admin(self):
+        user = User.objects.get(username="admin") 
+        repo = Repository.objects.get(slug='test-repo')
+
+        self.assertWriter(repo, user)
+
+    def test_reader(self):
+        user = User.objects.get(username="reader") 
+        repo = Repository.objects.get(slug='test-repo')
+
+        self.assertReader(repo, user)
+
+    def test_writer(self):
+        user = User.objects.get(username="writer") 
+        repo = Repository.objects.get(slug='test-repo')
+
+        self.assertWriter(repo, user)
+
+    def test_group_reader(self):
+        user = User.objects.get(username="group_reader") 
+        repo = Repository.objects.get(slug='test-repo')
+
+        self.assertReader(repo, user)
+
+    def test_writer(self):
+        user = User.objects.get(username="group_writer") 
+        repo = Repository.objects.get(slug='test-repo')
+
+        self.assertWriter(repo, user)
+
+    def test_owner(self):
+        user = User.objects.get(username="owner") 
+        repo = Repository.objects.get(slug='test-repo')
+
+        self.assertWriter(repo, user)
