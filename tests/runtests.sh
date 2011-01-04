c74e3f8b47d633c35c6d30dfd2a95db57641d92d
Ian Lewis <IanMLewis@gmail.com>
2011-01-04 11:40:06 +0900

    Added a script to run all tests

---
+++ tests/runtests.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+DJANGO_VERSIONS="1.1 1.2"
+HG_VERSIONS="1.3.1 1.4.3 1.5.4 1.6.3"
+
+set -e
+python bootstrap.py
+./bin/buildout
+
+set +e
+for DJANGO_VER in $DJANGO_VERSIONS
+do
+    for HG_VER in $HG_VERSIONS
+    do
+        ./bin/test-django-$DJANGO_VER-hg-$HG_VER
+    done
+done
